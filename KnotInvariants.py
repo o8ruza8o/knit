e967f6ab482947019bb18689c907eefe34ff4f1a
Ruza Markov <ruza.markov@gmail.com>
2014-07-29 09:03:46 -0700

    a bunch of corrections in linking number calculation plus mayavi plotting

--- KnotInvariants.py
+++ KnotInvariants.py
@@ -1,16 +1,17 @@
 import numpy as np
+from mayavi.mlab import *
 from scipy import integrate, interpolate, linalg
 
 def estNormedGradient(func, point, lambder = 1e-8):
-    dF = (func(point) - func(point + lambder)) / lambder
+    dF = (func(point + lambder / 2.0) - func(point - lambder / 2.0)) / lambder
     dF /= linalg.norm(dF)
     return dF
 
 def triplify(aray):
     return np.concatenate((aray, aray, aray))
 
-def crossDot(v1, v2):
-    return np.dot(np.cross(v1, v2), v2)
+def crossDot(v1, v2, v3):
+    return np.dot(v1, np.cross(v2, v3))
 
 twoPi = np.pi * 2
 
@@ -18,23 +19,33 @@ def computeLinkingNumber(pointSet1, pointSet2):
     # Make a spline of each of the input point sets, with periodic wrapping
     pts1 = np.concatenate((pointSet1, pointSet1))
     tht1 = np.linspace(-twoPi, twoPi, pts1.shape[0])
-    sp1 = interpolate.interp1d(tht1, pts1.T, kind="quadratic")
+    sp1 = interpolate.interp1d(tht1, pts1.T, kind="cubic")
 
+    # once = np.linspace(-np.pi, np.pi, 100)
+    # circle = sp1(once)
+    # plot3d(circle[0, :], circle[1, :], circle[2, :])
+
+    # grad = np.array([estNormedGradient(sp1, tht) for tht in once]).T
+    # quiver3d(circle[0, :], circle[1, :], circle[2, :], 
+    #          grad[0, :], grad[1, :], grad[2, :]) 
+    # show()
 
     pts2 = np.concatenate((pointSet2, pointSet2))
     tht2 = np.linspace(-twoPi, twoPi, pts2.shape[0])
-    sp2 = interpolate.interp1d(tht2, pts2.T, kind="quadratic")
+    sp2 = interpolate.interp1d(tht2, pts2.T, kind="cubic")
 
     def integrateMe(phi1, phi2):
         g1 = estNormedGradient(sp1, phi1)
         g2 = estNormedGradient(sp2, phi2)
 
-        dist = linalg.norm(g2 - g1)
-        cd = crossDot(g1, g2)
+        dif = sp2(phi2) - sp1(phi1)
+        dist3 = linalg.norm(dif)**3
+        link = crossDot(dif, g1, g2) / (np.pi * dist3 * 4.0)
 
-        return (dist**3/dist) * cd
+        return link
 
-    return integrate.dblquad(integrateMe, 0, twoPi, lambda x: 0, lambda y: twoPi)
+    return integrate.dblquad(integrateMe, -np.pi, np.pi, 
+                             lambda x: -np.pi, lambda y: np.pi)
 
 
 if __name__ == "__main__":
@@ -46,12 +57,15 @@ if __name__ == "__main__":
     zs1 = np.ones_like(p)
     xyz1 = np.c_[xs1, ys1, zs1]
 
-
     r2 = 2
     xs2 = r2 * np.cos(p) + 1
     ys2 = np.ones_like(p) * 0
     zs2 = r2 * np.sin(p)
     xyz2 = np.c_[xs2, ys2, zs2]
 
-    print computeLinkingNumber(xyz1, xyz2)
+    # plot3d(xs1, ys1, zs1, color=(0.0, 0.8, 0.8), tube_radius=0.1)
+    # plot3d(xs2, ys2, zs2, color=(0.8, 0.8, 0.0), tube_radius=0.1)
+    # savefig('circle.png')
+    # show()
 
+    print computeLinkingNumber(xyz1, xyz2)
