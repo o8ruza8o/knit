2fa68d9db92153fe7a867c8a3890b9c0c78dd7e4
Ruza Markov <ruza.markov@gmail.com>
2014-07-30 11:32:37 -0700

    functional (tho still slow) knot invariant

--- KnotInvariants.py
+++ KnotInvariants.py
@@ -2,15 +2,11 @@ import numpy as np
 from mayavi.mlab import *
 from scipy import integrate, interpolate, linalg
 
-def estNormedGradient(func, point, lambder = 1e-8):
-    dF = (func(point + lambder / 2.0) - func(point - lambder / 2.0)) / lambder
-    dF /= linalg.norm(dF)
+def estNormedGradient(func, point, epsilon= 1e-5):
+    dF = (func(point + epsilon / 2.0) - func(point - epsilon / 2.0)) / epsilon
     return dF
 
-def triplify(aray):
-    return np.concatenate((aray, aray, aray))
-
-def crossDot(v1, v2, v3):
+def dotCross(v1, v2, v3):
     return np.dot(v1, np.cross(v2, v3))
 
 twoPi = np.pi * 2
@@ -38,34 +34,57 @@ def computeLinkingNumber(pointSet1, pointSet2):
         g1 = estNormedGradient(sp1, phi1)
         g2 = estNormedGradient(sp2, phi2)
 
-        dif = sp2(phi2) - sp1(phi1)
-        dist3 = linalg.norm(dif)**3
-        link = crossDot(dif, g1, g2) / (np.pi * dist3 * 4.0)
+        dist = sp1(phi1) - sp2(phi2)
+        dist3 = linalg.norm(dist)**3
+        link = dotCross(dist, g1, g2) / (np.pi * dist3 * 4.0)
 
         return link
 
     return integrate.dblquad(integrateMe, -np.pi, np.pi, 
-                             lambda x: -np.pi, lambda y: np.pi)
+                             lambda x: -np.pi, lambda y: np.pi, 
+                             epsabs = 1e-3, epsrel = 1e-3)
 
 
 if __name__ == "__main__":
+    # print integrate.dblquad(lambda x, y: x, 0, 1, lambda x: 0, lambda y: 5)
+
     p = np.linspace(0, twoPi, 100)
 
-    r1 = 2
-    xs1 = r1 * np.cos(p)
-    ys1 = r1 * np.sin(p)
-    zs1 = np.ones_like(p)
-    xyz1 = np.c_[xs1, ys1, zs1]
+    # r1 = 2
+    # xs1 = r1 * np.cos(p)
+    # ys1 = r1 * np.sin(p)
+    # zs1 = np.ones_like(p) * 0
+    # xyz1 = np.c_[xs1, ys1, zs1]
 
-    r2 = 2
-    xs2 = r2 * np.cos(p) + 1
-    ys2 = np.ones_like(p) * 0
-    zs2 = r2 * np.sin(p)
-    xyz2 = np.c_[xs2, ys2, zs2]
+    # r2 = 2
+    # xs2 = r2 * np.cos(p) + 1
+    # ys2 = np.ones_like(p) * 0
+    # zs2 = r2 * np.sin(p)
+    # xyz2 = np.c_[xs2, ys2, zs2]
 
     # plot3d(xs1, ys1, zs1, color=(0.0, 0.8, 0.8), tube_radius=0.1)
     # plot3d(xs2, ys2, zs2, color=(0.8, 0.8, 0.0), tube_radius=0.1)
-    # savefig('circle.png')
+    # savefig('knot.png')
     # show()
+
+    a = 1.8
+    x0 = 0.1
+    y0 = 0.1
+    z0 = -0.1
+
+    xs1 = (a +  np.cos(5 * p)) * np.cos(2 * p)
+    ys1 = (a +  np.cos(5 * p)) * np.sin(2 * p)
+    zs1 = np.sin(5 * p)
+    xyz1 = np.c_[xs1, ys1, zs1]
+
+    xs2 = x0 - a * np.cos(p)
+    ys2 = y0 + a * np.sin(p)
+    zs2 = z0 * np.ones_like(p)
+    xyz2 = np.c_[xs2, ys2, zs2]
+
+    plot3d(xs1, ys1, zs1, color=(0.0, 0.8, 0.8), tube_radius=0.1)
+    plot3d(xs2, ys2, zs2, color=(0.8, 0.8, 0.0), tube_radius=0.1)
+    savefig('knot.png')
+    show()
 
     print computeLinkingNumber(xyz1, xyz2)
